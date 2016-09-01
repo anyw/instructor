4ade6d8db74a728b1159bcb9a2ae147538a6e33e
anyw <ano46379@autistici.org>
2016-09-01 15:08:12 +0200

    Update api.php
    ok

--- services/api.php
+++ services/api.php
@@ -5,7 +5,7 @@
 	
 		public $data = "";
 		
-		const DB_SERVER = "172.30.255.47";
+		const DB_SERVER = "172.30.66.82";
 		const DB_USER = "instructor";
 		const DB_PASSWORD = "password";
 		const DB = "instructor";
